8a63d313d87f58e42317a2e00c0f4d0de525daad
Richard Miranda <richarddmiranda@gmail.com>
2024-02-29 17:14:43 +0100

    restful routes, dashboard page, admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,44 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  
+  get 'apply', to: 'pages#apply' # new user application form
+  get 'start', to: 'pages#start' # new user onboarding
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'signup' }
-  get 'apply', to: 'pages#apply', as: 'apply'
-  get 'start', to: 'pages#start', as: 'start'
+  devise_for :users
   get 'logout', to: 'pages#logout', as: 'logout'
 
   resources :subscribe, only: [:index]
-  resources :dashboard, only: [:index]
+  # resources :dashboard, only: [:index]
+  get 'dashboard', to: 'dashboard#index', as: :dashboard_index
+
+  get 'stakeholder_updates/new', to: 'stakeholder_updates#new'
+
   resources :account, only: [:index, :update]
-  resources :billing_portal, only: [:new, :create]
-  resources :blog_posts, controller: :blog_posts, path: "blog", param: :slug
+  resources :billing_portal, only: [:create]
+  match '/billing_portal' => 'billing_portal#create', via: [:get]
+  match '/cancel' => 'billing_portal#destroy', via: [:get]
 
   # static pages
-  pages = %w[
+  pages = %w(
     privacy terms
-  ]
+  )
 
   pages.each do |page|
-    get "/#{page}", to: "pages##{page}", as: page.gsub('-', '_').to_s
+    get "/#{page}", to: "pages##{page}", as: "#{page.gsub('-', '_')}"
   end
 
   namespace :admin do
     get '/', to: 'pages#dashboard'
   end
-  
+
+  # admin panels
+  # authenticated :user, -> user { user.admin? } do
+  #   namespace :admin do
+  #     resources :dashboard, only: [:index]
+  #     resources :impersonations, only: [:new]
+  #     resources :users, only: [:edit, :update, :destroy]
+  #   end
+  #
+  #   # convenience helper
+  #   get 'admin', to: 'admin/dashboard#index'
+  # end
 end
